3cfe5d440f4c1a70878b2dcd3a498c189747f84c
Timofey Epanchintsev <t.epanchintsev@carbonsoft.ru>
2021-09-13 16:26:14 +0500

    Fixed(main): stable function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 mod bot;
 
-use anyhow::Result;
 use bot::qbot::QbitBot;
 use futures_util::stream::StreamExt;
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() {
     let mut qbot = QbitBot::new().await;
     println!("QbitBot launched");
     let mut updates_stream = Box::pin(qbot.rbot.incoming_updates(None, None));
-    while let Some(update) = updates_stream.next().await.transpose()? {
-        let res = qbot.proccess_message(update).await;
-        println!("{:#?}", res)
+    loop {
+        match updates_stream.next().await.transpose() {
+            Ok(update_opt) => {
+                if let Some(update) = update_opt {
+                    let res = qbot.proccess_message(update).await;
+                    println!("{:#?}", res)
+                } else {
+                    println!("None")
+                }
+            }
+            Err(err) => println!("{:#?}", err),
+        };
     }
-    Ok(())
 }
