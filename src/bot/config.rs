440f32e91089e41d6b712cb1b0a336b8c6cc75f3
Timofey Epanchintsev <epanchee@gmail.com>
2021-10-16 21:43:25 +0500

    Refactoring

--- src/bot/config.rs
+++ src/bot/config.rs
@@ -1,9 +1,11 @@
+use std::collections::HashSet;
+
 #[derive(Debug, Clone)]
 pub struct QbConfig {
     pub location: String,
     pub user: String,
     pub password: String,
-    pub admins: Vec<String>,
+    pub admins: HashSet<String>,
 }
 
 impl QbConfig {
@@ -12,7 +14,11 @@ impl QbConfig {
             location: dotenv::var("QBLOCATION").expect(&format!(dotenv_err!(), "QBLOCATION")),
             user: dotenv::var("QBUSER").expect(&format!(dotenv_err!(), "QBUSER")),
             password: dotenv::var("QBPASS").expect(&format!(dotenv_err!(), "QBPASS")),
-            admins: vec![dotenv::var("ADMIN").expect(&format!(dotenv_err!(), "ADMIN"))],
+            admins: dotenv::var("ADMIN")
+                .expect(&format!(dotenv_err!(), "ADMIN"))
+                .split(' ')
+                .map(String::from)
+                .collect(),
         }
     }
 }
